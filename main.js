33de14c3075b88206634fbe847a681c77b5cdbdc
carolinafreire <68876784+carolinafreire@users.noreply.github.com>
2022-08-30 15:40:58 -0300

    fnalizado

--- main.js
+++ main.js
@@ -1,5 +1,52 @@
-function tocaSomPom() {
-  document.querySelector("#som_tecla_pom").play();
+function tocaSom(idTecla) {
+  const elemento = document.querySelector(idTecla).play();
+
+  if (elemento && elemento.localName === "audio") {
+    elemento.play();
+  }
+  else {
+    console.log("Tecla não encontrada")
+  }
 }
 
-document.querySelector(".tecla_pom").onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll(".tecla")
+
+let contador = 0;
+
+/*while (contador < listaDeTeclas.length) {
+
+  const tecla = listaDeTeclas[contador]; 
+
+  const instrumento = tecla.classList[1];
+
+  const idAudio = `#som_${instrumento}`;
+
+  tecla.onclick = function () {
+    tocaSom(idAudio)
+  }
+
+  contador = contador + 1;
+}*/
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador]; 
+
+  const instrumento = tecla.classList[1];
+
+  const idAudio = `#som_${instrumento}`;
+
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  }
+
+  tecla.onkeydown = function (clique) {
+    if (clique.code === "Space" || clique.code === "Enter") {
+      tecla.classList.add("ativa");
+    }
+  }
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
+}
+
+}
